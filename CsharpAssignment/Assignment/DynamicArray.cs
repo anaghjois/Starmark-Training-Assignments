0ae79e2b0a3bbf94b93d9368eeb45bfbb77be14f
anaghjois <76957947+anaghjois@users.noreply.github.com>
2023-01-03 09:25:41 +0530

    Add files via upload

---
+++ CsharpAssignment/Assignment/DynamicArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assignment
+{
+    class DynamicArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of an array");
+            int Size = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the common type for the data types");
+            string TypeName = Console.ReadLine();
+            Type type = Type.GetType(TypeName, true, true);
+            Array myArray = Array.CreateInstance(type, Size);
+            for (int i = 0; i < Size; i++)
+            {
+                Console.WriteLine($"Enter the value of the {type.Name}");
+                string enteredValue = Console.ReadLine();
+                object convertedValue = Convert.ChangeType(enteredValue, type);
+                myArray.SetValue(convertedValue, i);
+            }
+            Console.WriteLine("All Values are");
+            foreach  (object values in myArray)
+            {
+                Console.WriteLine(values);
+            }
+        }
+    }
+}
